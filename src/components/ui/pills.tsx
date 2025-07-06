12f409f4ba031c74c5fcdfbc5215e156a9f4fae8
jaivanshchawla <jaivansh.chawla@gmail.com>
2025-07-06 23:33:39 +0530

    linux

--- src/components/ui/pills.tsx
+++ src/components/ui/pills.tsx
@@ -51,7 +51,7 @@ const brands = [
     hoverClass: "group-hover:scale-75 group-hover:text-gray-500",
   },
   {
-    name: "BuySellAds",
+    name: "Linux (Ubuntu)",
     className: "bg-gray-200",
     hoverClass: "group-hover:scale-75 group-hover:text-gray-500",
   },

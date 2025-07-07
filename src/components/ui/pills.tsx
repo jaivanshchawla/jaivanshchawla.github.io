a59ca1885e939b568b62e2cc00725171bd58d993
jaivanshchawla <jaivansh.chawla@gmail.com>
2025-07-07 11:57:02 +0530

    kuber

--- src/components/ui/pills.tsx
+++ src/components/ui/pills.tsx
@@ -72,7 +72,7 @@ const brands = [
     hoverClass: "group-hover:scale-75 group-hover:text-gray-500",
   },
   {
-    name: "Docker",
+    name: "Kubernetes",
     className: "bg-gray-200",
     hoverClass: "group-hover:scale-75 group-hover:text-gray-500",
   },

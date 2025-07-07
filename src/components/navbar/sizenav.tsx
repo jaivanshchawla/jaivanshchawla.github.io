6f861b9b4e908cc704f41da7bcc2bccf11e789af
jaivanshchawla <jaivansh.chawla@gmail.com>
2025-07-07 16:17:16 +0530

    navbar logo

--- src/components/navbar/sizenav.tsx
+++ src/components/navbar/sizenav.tsx
@@ -247,7 +247,7 @@ export const NavbarLogo = () => (
     className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
   >
     <img
-      src="/nordtech/assets/navlogo.png"
+      src="/assets/navlogo.png"
       alt="logo"
       width={30}
       height={30}

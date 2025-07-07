bf4d2640dc0ad28b0d437326def3c0ab7b4c0f38
jaivanshchawla <jaivansh.chawla@gmail.com>
2025-07-07 11:59:22 +0530

    AI

--- src/components/ui/pills.tsx
+++ src/components/ui/pills.tsx
@@ -77,7 +77,7 @@ const brands = [
     hoverClass: "group-hover:scale-75 group-hover:text-gray-500",
   },
   {
-    name: "Docker",
+    name: "AI",
     className: "bg-gray-200",
     hoverClass: "group-hover:scale-75 group-hover:text-gray-500",
   },

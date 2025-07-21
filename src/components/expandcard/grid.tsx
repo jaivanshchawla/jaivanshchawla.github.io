26a31bf3d25564154acba2582830b2053582d8a4
jaivanshchawla <jaivansh.chawla@gmail.com>
2025-07-21 23:24:19 +0530

    text updates

--- src/components/expandcard/grid.tsx
+++ src/components/expandcard/grid.tsx
@@ -186,15 +186,7 @@ const cards = [
     ctaColor: "#E53935", 
     content: () => (
       <p>
-        Lana Del Rey, an iconic American singer-songwriter, is celebrated for
-        her melancholic and cinematic music style. Born Elizabeth Woolridge
-        Grant in New York City, she has captivated audiences worldwide with her
-        haunting voice and introspective lyrics. <br /> <br /> Her songs often
-        explore themes of tragic romance, glamour, and melancholia, drawing
-        inspiration from both contemporary and vintage pop culture. With a
-        career that has seen numerous critically acclaimed albums, Lana Del Rey
-        has established herself as a unique and influential figure in the music
-        industry, earning a dedicated fan base and numerous accolades.
+        
       </p>
     ),
   },
@@ -210,7 +202,7 @@ const cards = [
         Babbu Maan, a legendary Punjabi singer, is renowned for his soulful
         voice and profound lyrics that resonate deeply with his audience. Born
         in the village of Khant Maanpur in Punjab, India, he has become a
-        cultural icon in the Punjabi music industry. <br /> <br /> His songs
+        cultural icon in the Punjabi music industry. <hr></hr> His songs
         often reflect the struggles and triumphs of everyday life, capturing the
         essence of Punjabi culture and traditions. With a career spanning over
         two decades, Babbu Maan has released numerous hit albums and singles
